e881bcb24682162c86288b2fd2c126c643747af6
Aaron Orozco Fernandez <aorozco@hiberus.com>
2024-03-06 19:05:31 +0100

    Primera version de vista de perfil de usuario

--- tfg-project/src/components/Login/LoginForm.js
+++ tfg-project/src/components/Login/LoginForm.js
@@ -2,6 +2,7 @@ import './LoginForm.css';
 import es_flag from '../../assets/images/es_flag.png';
 import en_flag from '../../assets/images/en_flag.jpg';
 import {useTranslation} from "react-i18next";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const CLIENT_ID = "b771595a6c15c6653d02";
 
@@ -25,8 +26,9 @@ function Login(){
                     <h1>{t('login.welcome')}</h1>
                     <h2>{t('login.joinUs')}</h2>
                     <button type="submit" onClick={loginWithGitHub}>
-                        {t('login.login')}
+                    {t('login.login')} <i class="fa fa-github"></i> 
                     </button>
+        
                     <div className="translation">
                         <button type="submit" onClick={() => handleChangeLanguage("es")}>
                             <img src={es_flag} alt="esbutton" />
--- tfg-project/src/components/NavigationDrawer/options/ProfileView/ProfileView.js
+++ tfg-project/src/components/NavigationDrawer/options/ProfileView/ProfileView.js
@@ -1,13 +1,44 @@
 import * as React from 'react';
+import './ProfileView.css';
+import { useEffect,useState } from 'react';
 
-
+import TextField from '@mui/material/TextField';
 
 function ProfileView() {
+  const [userData, setUserData] = useState({});
 
+  useEffect(() => {
+    getUserData();
+    }, []);
 
+    async function getUserData() {
+      await fetch( "http://localhost:4000/getUserData", {
+        method: "GET",
+          headers: {
+              "Authorization" : "Bearer "+ localStorage.getItem("accessToken")
+          }
+      }).then((response) => {       
+         return response.json();
+      }).then((data) => {
+        console.log(data);
+          setUserData(data);
+      })
+    }
+    
+    const estiloTextField = {
+      pointerEvents: 'none',
+    };
 
   return (
-    <div className='hello'>
+    <div className='profileViewerDiv'>
+      <img src={userData.avatar_url} alt="esbutton" />
+      <TextField
+          required
+          id="outlined-required"
+          label="Nombre"
+          value="hola"
+          style={estiloTextField}
+          />
     </div>
     
   );
