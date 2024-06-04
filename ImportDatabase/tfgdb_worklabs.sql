e4349399532b9b96a777a774e088934a2dd3551c
Aarón Orozco Fernández <102319391+aaronof17@users.noreply.github.com>
2024-06-04 18:52:55 +0200

    Add files via upload

---
+++ ImportDatabase/tfgdb_worklabs.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: tfgdb
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `worklabs`
+--
+
+DROP TABLE IF EXISTS `worklabs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `worklabs` (
+  `worklabID` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `initialdate` datetime NOT NULL,
+  `finaldate` datetime NOT NULL,
+  `percentage` int NOT NULL,
+  `labgroupNameFK` varchar(45) NOT NULL,
+  `teacherIDFK` int NOT NULL,
+  `active` tinyint NOT NULL,
+  PRIMARY KEY (`worklabID`),
+  KEY `labgroupNameFK_idx` (`labgroupNameFK`),
+  KEY `teacherFK_idx` (`teacherIDFK`),
+  CONSTRAINT `labgroupNameFK` FOREIGN KEY (`labgroupNameFK`) REFERENCES `labgroups` (`Name`) ON DELETE CASCADE,
+  CONSTRAINT `teacherFK` FOREIGN KEY (`teacherIDFK`) REFERENCES `teachers` (`TeacherID`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=97 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `worklabs`
+--
+
+LOCK TABLES `worklabs` WRITE;
+/*!40000 ALTER TABLE `worklabs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `worklabs` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-02 16:09:26
